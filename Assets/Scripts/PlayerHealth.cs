4ae6506325156a5771707063fdfc9d3a1fd35f1e
Supanat166 <supanat.siem@bumail.net>
2025-05-04 00:49:59 +0700

    Add PlayerHealth

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    private int currentHealth;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        Debug.Log("Player took damage: " + amount + " | Current HP: " + currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Player Died!");
+        // ทำลายตัวละครหรือลด scene ได้ตามต้องการ
+    }
+}
